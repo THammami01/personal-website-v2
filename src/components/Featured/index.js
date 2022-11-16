f8bee502631e7c4d7bd4cdfda0a4ea9fe011feb6
Tarek Hammami <tarekhammamix01@gmail.com>
2022-11-16 04:52:16 +0100

    chore: add Dr. office MS to featured projects

--- src/components/Featured/index.js
+++ src/components/Featured/index.js
@@ -71,7 +71,7 @@ const Featured = () => {
                 </div>
 
                 <StyledProjectImgWrapper>
-                  <a href={external || github || '#'}>
+                  <a href={external || github || '#'} rel="noreferrer" target="_blank">
                     <div className="img-wrapper">
                       <div className="img-cont" />
                       <StyledProjectImage src={cover} alt={title} className="img" />
--- src/config/featured.js
+++ src/config/featured.js
@@ -6,7 +6,7 @@ module.exports = [
     github: 'https://github.com/THammami01/institute-records-ms',
     external: 'https://github.com/THammami01/institute-records-ms/blob/main/SGRN.exe',
     descriptionHtml:
-      'A management system that allows easy access to students data and documents with a few clicks or a quick search.<br /><br /><a target="_blank" rel="noreferrer" href="https://github.com/THammami01/institute-records-ms/blob/main/SGRN.exe">Download the app (only for Windows)</a>',
+      'A management system that allows easy access to students data and documents with a few clicks or a quick search.<br /><a target="_blank" rel="noreferrer" href="https://github.com/THammami01/institute-records-ms/blob/main/SGRN.exe">Download the app (only for Windows)</a>',
     techs: ['JDK 11', 'JavaFX', 'MariaDB', 'Scene Builder'],
   },
   {
@@ -14,7 +14,7 @@ module.exports = [
     cover: '/bg-placeholder.png',
     github: 'https://github.com/THammami01',
     descriptionHtml:
-      'An extension for Chromium-based broswers that skips video ads and hides other 5 types of banner ads on YouTube, as well as displays statistics for the skipped ones (number and duration per past day, month and the total).<br /><br /><a target="_blank" rel="noreferrer" href="https://github.com/THammami01/youtube-ad-blocker">Download the extension</a>',
+      'An extension for Chromium-based broswers that skips video ads and hides other 5 types of banner ads on YouTube, as well as displays statistics for the skipped ones (number and duration per past day, month and the total).<br /><a target="_blank" rel="noreferrer" href="https://github.com/THammami01/youtube-ad-blocker">Download the extension</a>',
     techs: ['JavaScript', 'Tailwind CSS', 'Chrome APIs'],
   },
   {
@@ -23,12 +23,13 @@ module.exports = [
     github: 'https://github.com/THammami01/rectn',
     external: 'https://rectn.herokuapp.com',
     descriptionHtml:
-      'A website that recommends movies and series based on data from The Movie Database, with a volatile auth system and the ability to save your favorite movies and genres.<br /><br /><a target="_blank" rel="noreferrer" href="https://rectn.herokuapp.com">Visit the website</a>',
+      'A website that recommends movies and series based on data from The Movie Database, with a volatile auth system and the ability to save your favorite movies and genres.<br /><a target="_blank" rel="noreferrer" href="https://rectn.herokuapp.com">Visit the website</a>',
     techs: ['Node.js', 'Python', 'React', 'Flask', 'Express'],
   },
   {
     title: 'Sci-Docs Graph App',
     cover: '/bg-placeholder.png',
+    github: 'https://github.com/THammami01/graph-app-server',
     // external: 'https://thammami01.github.io',
     // descriptionHtml:
     //   'Beautiful and pixel perfect React Icon Library, <a target="_blank" rel="noreferrer" href="https://piqodesign.gumroad.com/l/iconly">Iconly</a> is one of the options that is being used by designers and developers today, so I decided to create a library for React / Next.js / Gatsby that facilitates its use and that also allows us to customize any icon according to our needs.  <a target="_blank" rel="noreferrer" href="https://dev.to/jrgarciadev/beautiful-and-pixel-perfect-react-icon-library-2fnd">Blog Post</a>',
@@ -36,11 +37,11 @@ module.exports = [
       `A web-based desktop app that saves scientific files in a graph database and cloud storage. Each file is processed, uploaded to GDrive, and can be visualized in a graph.<br />` +
       `PDF files contents are extracted; every image and page's text is uploaded and have a node in the graph. ` +
       `MS Word files are converted to PDF and their contents are extracted as well. ` +
-      `If some MS Excel files column names match some templates, a chart will be generated using its values and they will have nodes in the graph too.<br /><br />` +
+      `If some MS Excel files column names match some templates, a chart will be generated using its values and they will have nodes in the graph too.<br />` +
       `<a target="_blank" rel="noreferrer" href="https://github.com/THammami01/graph-app-server">View the backend's source code</a>` +
       `<a target="_blank" rel="noreferrer" href="https://github.com/THammami01/graph-app-client">View the fronend's source code</a>`,
     // `<a target="_blank" rel="noreferrer" href="https://thammami01.github.io/esi-report-2022-eyp-report">View on my end of year internship report for more</a>`,
-    techs: ['React', 'Electron', 'Python', 'FastAPI', 'Neo4j'],
+    techs: ['Python', 'FastAPI', 'Neo4j', 'Node.js', 'React', 'Electron'],
   },
   {
     title: 'Typing Speed Increaser',
@@ -50,7 +51,18 @@ module.exports = [
     // descriptionHtml:
     //   'Beautiful and pixel perfect React Icon Library, <a target="_blank" rel="noreferrer" href="https://piqodesign.gumroad.com/l/iconly">Iconly</a> is one of the options that is being used by designers and developers today, so I decided to create a library for React / Next.js / Gatsby that facilitates its use and that also allows us to customize any icon according to our needs.  <a target="_blank" rel="noreferrer" href="https://dev.to/jrgarciadev/beautiful-and-pixel-perfect-react-icon-library-2fnd">Blog Post</a>',
     descriptionHtml:
-      'An application that allows you to practice and improve your typing by displaying your speed in real time as you write, as well as having the ability to change the theme, duration of typing, and adding custom texts.<br /><br /><a target="_blank" rel="noreferrer" href="https://github.com/THammami01/typing-speed-increaser">Download the app (only for Windows)</a>',
+      'An application that allows you to practice and improve your typing by displaying your speed in real time as you write, as well as having the ability to change the theme, duration of typing, and adding custom texts.<br /><a target="_blank" rel="noreferrer" href="https://github.com/THammami01/typing-speed-increaser">Download the app (only for Windows)</a>',
     techs: ['Python', 'Tkinter', 'Shelve'],
+  },
+  {
+    title: 'Dr. Office MS',
+    cover: '/bg-placeholder.png',
+    github: 'https://github.com/THammami01/dr-office-ms',
+    // external: 'https://thammami01.github.io',
+    // descriptionHtml:
+    //   'Beautiful and pixel perfect React Icon Library, <a target="_blank" rel="noreferrer" href="https://piqodesign.gumroad.com/l/iconly">Iconly</a> is one of the options that is being used by designers and developers today, so I decided to create a library for React / Next.js / Gatsby that facilitates its use and that also allows us to customize any icon according to our needs.  <a target="_blank" rel="noreferrer" href="https://dev.to/jrgarciadev/beautiful-and-pixel-perfect-react-icon-library-2fnd">Blog Post</a>',
+    descriptionHtml:
+      'A web-based desktop application for a doctor and their secretary to use for patient data management; to search, filter, paginate, back up and restore records.<br /><a target="_blank" rel="noreferrer" href="https://github.com/THammami01/dr-office-ms">Download the app (only for Windows)</a>',
+    techs: ['Node.js', 'Angular', 'PrimeNG', 'Electron', 'MySQL'],
   },
 ];
